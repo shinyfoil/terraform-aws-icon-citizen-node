ddf0469c002742e9e15b97f6165f31a0b3615998
robc-io <robc.io.opensource@gmail.com>
2019-08-26 00:33:39 -0700

    code dump

---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "region" {
+  description = "The region you are deploying into"
+}
+
+variable "environment" {
+  description = "The environment that generally corresponds to the account you are deploying into."
+}
+
+variable "resource_group" {
+  description = "The grouping that is conventionally used to name resources"
+}
+
+variable "tags" {
+  description = "Tags that are appended"
+  type        = map(string)
+}
+
+variable "terraform_state_region" {
+  description = "AWS region used for Terraform states"
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "instance_type" {
+}
+
+variable "root_volume_size" {
+}
+
+variable "volume_path" {
+}
+
+//variable "local_private_key" {} # TODO Only needed for remote calls but commented out now
+
+variable "efs_directory" {
+}
+
+variable "azs" {
+  description = "The availablity zones to deploy each ebs volume into."
+  type        = list(string)
+}
+
+variable "ebs_volume_size" {
+  description = "...."
+}
+
+//-----
+
+variable "key_name" {}
+variable "public_key" {}
+//TODO: Hide this^^^
+
+variable "security_groups" {
+  type = list
+}
+variable "subnet_id" {}
+
+//variable "file_system_id" {
+//  description = "The EFS file system id"
+//}
