2984201c785bf69dc305bb5afb587f7ba47c3bd0
robc-io <robc.io.opensource@gmail.com>
2019-09-15 01:57:19 -0700

    adding depends on

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ module "instance_id" {
   source = "matti/resource/shell"
   command = var.spot_price == 0 ? "echo no-waiting" : format("aws ec2 wait spot-instance-request-fulfilled --spot-instance-request-ids %s && aws ec2 describe-spot-instance-requests --spot-instance-request-ids %s | jq -r '.SpotInstanceRequests[].InstanceId'", aws_spot_instance_request.this.*.id[0], aws_spot_instance_request.this.*.id[0])
 
-  depends_on = []
+  depends_on = [null_resource.wait_on_startup]
 }
 
 
